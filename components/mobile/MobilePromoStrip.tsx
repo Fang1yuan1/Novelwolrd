9ecfd8c4c34de3f0a48fdc3cd43608cb0a0258c6
Fang1yuan1 <li666qi@gmail.com>
2026-09-19 08:33:07 +0300

    Last Sync: 2026-09-19 08:33 (Mobile)

--- components/mobile/MobilePromoStrip.tsx
+++ components/mobile/MobilePromoStrip.tsx
@@ -1,15 +1,13 @@
 export default function MobilePromoStrip() {
   return (
-    <section className="mobile-reference-card mobile-reference-promo">
-      <span className="mobile-reference-promo-icon" aria-hidden="true">
-        {/* eslint-disable-next-line @next/next/no-img-element */}
-        <img src="/brand/logo.png" alt="" className="h-full w-full object-cover" />
-      </span>
-      <div className="mobile-reference-promo-copy">
+    <section className="mobile-reference-card nw-promo">
+      {/* eslint-disable-next-line @next/next/no-img-element */}
+      <img src="/brand/app-icon.png" alt="" aria-hidden="true" className="nw-promo-icon" />
+      <div className="nw-promo-copy">
         <strong>اقرأ مجانًا على موقعنا</strong>
         <span>مكتبة كبيرة من الروايات والفصول الجديدة</span>
       </div>
-      <a href="/categories" className="mobile-reference-promo-button">سجل الآن</a>
+      <a href="/categories" className="nw-promo-button">سجل الآن</a>
     </section>
   );
 }
